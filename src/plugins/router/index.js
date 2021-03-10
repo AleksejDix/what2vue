a76e9fdc48203002e0a1516c9676557b5242aedc
Aleksej Dix <git@aleksejdix.com>
2021-03-10 10:18:09 +0100

    router: remove scrollBehavior

--- src/plugins/router/index.js
+++ src/plugins/router/index.js
@@ -8,16 +8,6 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   mode: 'history',
   routes,
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return {
-        x: 0,
-        y: 0,
-      }
-    }
-  },
 })
 router.beforeEach((to, from, next) => {
   if (from.name !== null) {
